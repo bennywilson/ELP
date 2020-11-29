3c3d17f6a984c15c1778be868646b1bcae366f67
bwilson <bennywilson@benny-wilson.com>
2020-11-29 13:39:17 -0500

    Set up basic structure and BP plumbing for destructibles

---
+++ Oxi/Source/Oxi/OxiDestructibleActor.cpp
@@ -0,0 +1,76 @@
+// ELP 2020
+
+#include "OxiDestructibleActor.h"
+
+UOxiDamageInterface::UOxiDamageInterface(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+float IOxiDamageInterface::TakeDamage_Implementation(const float DamageAmount, const AActor* DamageCauser)
+{
+	UE_LOG(LogTemp, Log, TEXT("TakeDamage_Implementation() - Finally made it.  Sheesh!"));
+	return 0.0f;
+}
+
+AOxiDestructibleActor::AOxiDestructibleActor()
+{
+	DestructibleComponent = CreateDefaultSubobject<UOxiDestructibleComponent>(TEXT("FirstPersonCamera"));
+
+}
+
+UOxiDestructibleComponent::UOxiDestructibleComponent()
+{
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	/*BaseMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComponent"));
+	BaseMeshComponent->SetOnlyOwnerSee(false);
+	BaseMeshComponent->SetupAttachment(this);
+	BaseMeshComponent->bCastDynamicShadow = true;
+	BaseMeshComponent->CastShadow = true;
+	BaseMeshComponent->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
+	BaseMeshComponent->SetRelativeLocation(FVector(0.f, 0.f, 0.f));
+
+	DestructibleMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("DestructibleMeshComponent"));
+	DestructibleMeshComponent->SetOnlyOwnerSee(false);
+	DestructibleMeshComponent->SetupAttachment(this);
+	DestructibleMeshComponent->bCastDynamicShadow = true;
+	DestructibleMeshComponent->CastShadow = true;
+	DestructibleMeshComponent->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
+	DestructibleMeshComponent->SetRelativeLocation(FVector(0.f, 0.f, 0.f));*/
+}
+
+void UOxiDestructibleComponent::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+
+	if (BaseMeshComponent != nullptr)
+	{
+		BaseMeshComponent->SetHiddenInGame(false, true);
+	}
+
+	if (DestructibleMeshComponent != nullptr)
+	{
+		DestructibleMeshComponent->SetHiddenInGame(false, true);
+	}
+}
+
+bool UOxiDestructibleComponent::InitDestructibleComponent(UStaticMeshComponent* InBaseMeshComponent, USkeletalMeshComponent* InDestructibleMeshComponent)
+{
+	BaseMeshComponent = InBaseMeshComponent;
+	DestructibleMeshComponent = InDestructibleMeshComponent;
+
+	return true;
+}
+
+void UOxiDestructibleComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
+float UOxiDestructibleComponent::TakeDamage_Internal(const float DamageAmount, const AActor* DamageCauser)
+{
+	UE_LOG(LogTemp, Log, TEXT("Made it yo!"));
+	return 1.3f;
+}
---
+++ Oxi/Source/Oxi/OxiDestructibleActor.h
@@ -0,0 +1,66 @@
+// ELP 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "UObject/Interface.h"
+#include "OxiDestructibleActor.generated.h"
+
+
+UINTERFACE(BlueprintType)
+class UOxiDamageInterface : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+class IOxiDamageInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Oxi Damage")
+	float TakeDamage(const float DamageAmount, const AActor* DamageCauser);
+	virtual float TakeDamage_Implementation(const float DamageAmount, const AActor* DamageCauser);
+
+private:
+	virtual float TakeDamage_Internal(const float DamageAmount, const AActor* DamageCauser) = 0;
+};
+
+UCLASS(config = Game)
+class AOxiDestructibleActor : public AActor
+{
+	GENERATED_BODY()
+	AOxiDestructibleActor();
+
+protected:
+	UPROPERTY(BlueprintReadWrite, Category = Mesh)
+	class UOxiDestructibleComponent* DestructibleComponent;
+};
+
+UCLASS(Blueprintable, editinlinenew, meta = (BlueprintSpawnableComponent))
+class OXI_API UOxiDestructibleComponent : public USceneComponent, public IOxiDamageInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	UOxiDestructibleComponent();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction);
+
+	UFUNCTION(BlueprintCallable, Category = "Oxi Damage")
+	bool InitDestructibleComponent(UStaticMeshComponent* InBaseMeshComponent, USkeletalMeshComponent* InDestructibleMeshComponent);
+
+private:
+	virtual float TakeDamage_Internal(const float DamageAmount, const AActor* DamageCauser) override;
+
+	UStaticMeshComponent* BaseMeshComponent;
+	USkeletalMeshComponent* DestructibleMeshComponent;
+};
